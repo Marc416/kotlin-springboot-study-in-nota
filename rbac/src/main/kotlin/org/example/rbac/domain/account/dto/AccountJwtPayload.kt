73b0be4581d67965864e19ae3bcd45b4c929a7fe
Marc416 <juni416@naver.com>
2025-05-21 23:35:32 +0900

    feat : JWT Token Provider

---
+++ rbac/src/main/kotlin/org/example/rbac/domain/account/dto/AccountJwtPayload.kt
@@ -0,0 +1,15 @@
+package org.example.rbac.domain.account.dto
+
+data class AccountJwtPayload(
+    val accountId: Long,
+    val tenantKey: String,
+    val role: String,
+){
+    fun toMap(): Map<String, Any> {
+        return mapOf(
+            "accountId" to accountId,
+            "tenantKey" to tenantKey,
+            "role" to role
+        )
+    }
+}
