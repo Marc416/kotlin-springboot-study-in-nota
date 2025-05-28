fd8b965f3a6d4ad06ce2f5987895a7d52ba34030
Marc416 <juni416@naver.com>
2025-05-28 21:08:09 +0900

    test : 토큰 Claim 기능 Live로 테스트 코드 만들어보기

--- rbac/src/test/kotlin/org/example/rbac/application/common/JwtTokenProviderImplTest.kt
+++ rbac/src/test/kotlin/org/example/rbac/application/common/JwtTokenProviderImplTest.kt
@@ -3,7 +3,6 @@ package org.example.rbac.application.common
 import org.assertj.core.api.Assertions.assertThat
 import org.example.rbac.domain.common.JwtTokenProvider
 import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.assertThrows
 import kotlin.test.Test
 
 class JwtTokenProviderImplTest {
@@ -24,5 +23,14 @@ class JwtTokenProviderImplTest {
 
         // Assert
         assertThat(res).isFalse()
+    }
+
+    @Test
+    fun `인증토큰에 accountId tenantKey role 이 있다`(){
+        // Arrange
+        // (Live로 만들어보기)
+        // Action
+
+        // Assert
     }
 }
