4a09f361f59a1376794f888ef012f6d20943ba21
Marc416 <juni416@naver.com>
2025-05-28 21:24:45 +0900

    feat : 필요한 API만 시큐리티 열어주기

--- rbac/src/main/kotlin/org/example/rbac/application/config/security/SecurityConfig.kt
+++ rbac/src/main/kotlin/org/example/rbac/application/config/security/SecurityConfig.kt
@@ -3,17 +3,9 @@ package org.example.rbac.application.config.security
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer
-import org.springframework.security.config.annotation.web.configurers.FormLoginConfigurer
-import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer
-import org.springframework.security.config.annotation.web.configurers.SessionManagementConfigurer
-import org.springframework.security.config.http.SessionCreationPolicy
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
-import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
 
 @Configuration
@@ -28,7 +20,14 @@ class SecurityConfig{
 
         // 권한 규칙 작성
         http.authorizeHttpRequests { httpRequest ->
-            httpRequest.anyRequest().permitAll()
+            httpRequest.requestMatchers("/account/password/*").authenticated()
+            httpRequest.requestMatchers("/account/**").permitAll()
+            httpRequest.requestMatchers(
+                "/swagger/**",
+                "/swagger-ui/**",
+                "/api-docs/**"
+            ).permitAll()
+            httpRequest.anyRequest().authenticated()
         }
 
 
